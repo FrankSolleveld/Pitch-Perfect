105fb907e49a2159069a83e5176cd009200243c7
Frank Solleveld <fsolleveld@outlook.com>
2017-04-13 08:50:47 +0200

    02. AutoLayout and Buttons

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress!"
+        
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        
+        print("Stop recording button was pressed")
+        
     }
 
 
