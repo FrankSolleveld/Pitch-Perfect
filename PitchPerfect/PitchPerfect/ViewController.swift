9c628f425b93efffc27a88566639294d7d5e7f27
Frank Solleveld <fsolleveld@outlook.com>
2017-04-13 08:11:32 +0200

    00. Create Project

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Frank Solleveld on 13-04-17.
+//  Copyright © 2017 Frank Solleveld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
